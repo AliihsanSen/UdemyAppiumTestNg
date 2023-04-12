a4d0adf4543d30be6397b72a470f6817d233cd63
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-12 16:26:19 +0300

    12.04.2023

--- src/test/java/qa/mobile/BaseTest.java
+++ src/test/java/qa/mobile/BaseTest.java
@@ -1,4 +1,20 @@
 package qa.mobile;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
 public class BaseTest {
-}
+
+    @BeforeClass
+    public void beforeClass()  {}
+
+    @Test
+    public void test()  {}
+
+    @AfterClass
+    public void afterClass()  {}
+
+    }
