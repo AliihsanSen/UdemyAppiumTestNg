7e69660041b1697348eb8f8fd5403e0185dc4f00
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-12 13:39:01 +0300

    12.04.2023

---
+++ src/test/java/qa/mobile/test1.java
@@ -0,0 +1,20 @@
+package qa.mobile;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class test1 {
+
+    @BeforeClass
+    public void beforeClass(){
+    }
+
+    @Test
+    public void f(){
+    }
+
+    @AfterClass
+    public void afterClass(){
+    }
+}
