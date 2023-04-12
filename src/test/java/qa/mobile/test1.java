6a83c2b221235c1de63f5c3a491ea48eaad96909
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-12 13:50:33 +0300

    12.04.2023

--- src/test/java/qa/mobile/test1.java
+++ src/test/java/qa/mobile/test1.java
@@ -1,7 +1,14 @@
 package qa.mobile;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class test1 {
@@ -10,8 +17,29 @@ public class test1 {
     public void beforeClass(){
     }
 
+
     @Test
-    public void f(){
+    public void f() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        //  caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        caps.setCapability("avdLaunchTimeout", 180000);
+        caps.setCapability("appPackage", "io.appium.android.apis");
+        caps.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+        caps.setCapability(MobileCapabilityType.APP,
+                "/Users/tribe/Desktop/Projects/AppiumProjects/MyFirstAppiumProjectTestNG" +
+                        "/src/main/resources/ApiDemos-debug.apk");
+
+              //  ==> Yüklenmemiş uygulamayı mobil cihaza yüklemek için kullanılır.
+
+
+        URL url = new URL("http://0.0.0.0:4723/");
+
+        AppiumDriver driver = new AndroidDriver(url, caps);
+        System.out.println("Session Id : " + driver.getSessionId());
+
     }
 
     @AfterClass
