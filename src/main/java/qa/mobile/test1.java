97604174f018f0a0860ad8bcf718c4c89d2f28fe
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-04-12 15:07:35 +0300

    12.04.2023

--- src/main/java/qa/mobile/test1.java
+++ src/main/java/qa/mobile/test1.java
@@ -5,6 +5,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,10 +18,6 @@ import java.util.concurrent.TimeUnit;
 public class test1 {
 
     AppiumDriver driver;
-    WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
-    WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
-    WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
-    WebElement errTxt = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView"));
 
     @BeforeClass
     public void beforeClass() throws MalformedURLException {
@@ -48,8 +45,65 @@ public class test1 {
 
 
     @Test
-    public void Test01()   {
+    public void invalidUserName()   {
 
+        WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
+        WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
+        WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
+
+        usernameTxtFld.sendKeys("invalidusername");
+        passwordTxtFld.sendKeys("secret_sauce");
+        loginBtn.click();
+
+        WebElement errTxt = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"test" +
+                "-Error message\"]/android.widget.TextView"));
+
+        String actualErrTxt = errTxt.getAttribute("text");
+        System.out.println("actual Error Text = " + actualErrTxt);
+        String expectedErrTxt = "Username and password do not match any user in this service.";
+        Assert.assertEquals(actualErrTxt, expectedErrTxt);
+
+    }
+
+    @Test
+    public void invalidPassword(){
+
+        WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
+        WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
+        WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
+
+        usernameTxtFld.sendKeys("standard_user");
+        passwordTxtFld.sendKeys("invalidPassword");
+        loginBtn.click();
+
+        WebElement errTxt = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"test" +
+                "-Error message\"]/android.widget.TextView"));
+
+        String actualErrTxt = errTxt.getAttribute("text");
+        System.out.println("actual Error Text = " + actualErrTxt);
+        String expectedErrTxt = "Username and password do not match any user in this service.";
+        Assert.assertEquals(actualErrTxt, expectedErrTxt);
+
+    }
+
+    @Test
+    public void successfulLogin(){
+
+        WebElement usernameTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Username"));
+        WebElement passwordTxtFld = driver.findElement(AppiumBy.accessibilityId("test-Password"));
+        WebElement loginBtn = driver.findElement(AppiumBy.accessibilityId("test-LOGIN"));
+
+        usernameTxtFld.sendKeys("standard_user");
+        passwordTxtFld.sendKeys("secret_sauce");
+        loginBtn.click();
+
+        WebElement productTitle = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-" +
+                "desc=\"test-Cart drop zone\"]/android.view.ViewGroup/android.widget.TextView"));
+
+        String actualProductTitle = productTitle.getAttribute("text");
+        System.out.println("actualProductTitle = " + actualProductTitle);
+        String expectedProductTitle = "PRODUCT";
+        Assert.assertEquals(actualProductTitle, expectedProductTitle);
     }
 
     @AfterClass
